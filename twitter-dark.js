d740538d7f6dd6ffa800857b362cccc94271befe
723 <rb.natsumi@gmail.com>
2017-03-05 15:17:09 +0900

    1st commit

---
+++ twitter-dark.js
@@ -0,0 +1,6 @@
+$(function(){
+  $("body").css("background","#002663");
+  $(".tweet").css("background","#002663");
+  $(".tweet").css("color","#ffffff");
+  $(".fullname").css("color","#ffffff");
+});
