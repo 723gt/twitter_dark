feb4f3b2eea70a58621efb2a90d340f15e92f752
723 <rb.natsumi@gmail.com>
2017-03-05 16:19:02 +0900

    暗めの適応範囲拡大

--- twitter-dark.js
+++ twitter-dark.js
@@ -3,6 +3,8 @@ $(function(){
   setInterval(tweet_dark,3000);
   function tweet_dark(){
     $("body").css("background","#002663");
+    $(".Trends").css("background","#002663");
+    $(".DashboardProfileCard-content").css("background","#002663");
     $(".tweet").css("background","#002663");
     $(".missing-tweets-bar").css("background","#002663");
     $(".stream-item-activity-line").css("background","#002663");
