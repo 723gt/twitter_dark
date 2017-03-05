1de060ffc98290a5023d6a291d66ff37fa1daf0c
723 <rb.natsumi@gmail.com>
2017-03-05 15:35:11 +0900

    ３秒毎に適用

--- twitter-dark.js
+++ twitter-dark.js
@@ -1,6 +1,10 @@
 $(function(){
-  $("body").css("background","#002663");
-  $(".tweet").css("background","#002663");
-  $(".tweet").css("color","#ffffff");
-  $(".fullname").css("color","#ffffff");
+  tweet_dark();
+  setInterval(tweet_dark,3000);
+  function tweet_dark(){
+    $("body").css("background","#002663");
+    $(".tweet").css("background","#002663");
+    $(".tweet").css("color","#ffffff");
+    $(".fullname").css("color","#ffffff");
+  }
 });
